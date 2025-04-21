2ae055024aa57f1f9d1e73aee5d99431206825c4
Jitendra Behera <jb.jitu@live.com>
2025-04-21 11:38:17 +0530

    changes

--- 5-ng-hybrid-index/src/app/app.module.ts
+++ 5-ng-hybrid-index/src/app/app.module.ts
@@ -13,6 +13,9 @@ import { UpgradeModule } from '@angular/upgrade/static';
 export class AppModule implements DoBootstrap {
   constructor(private upgrade: UpgradeModule) {}
   ngDoBootstrap() {
+    // in below code product-list will load inside angularjs-root element even you declare muliple product-list out side of angularjs-root div
+    // if not declare how may times product-list declared in index html that many times it will be load.
+    // for that you can use below commented code this.upgrade.bootstrap(document.body, ['legacyApp'], { strictDi: true });
     const element = document.getElementById('angularjs-root');
     if (element) {
       this.upgrade.bootstrap(element, ['legacyApp'], { strictDi: true });
